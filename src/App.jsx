3900d74767d8bbdda9f12706f2240a66cf93c577
NicolasPanis <nicolaspanis12@gmail.com>
2023-03-28 11:21:14 +0200

    New commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import './components/PokemonCard.jsx'
+import PokemonCard from './components/PokemonCard.jsx'
+
+function App() {
+  return (
+    <div>
+      <PokemonCard />
+    </div>
+    );
+}
+
+export default PokemonCard;
