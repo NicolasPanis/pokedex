d22b0eaa71f50d800d27dad45d206790f7d0974c
NicolasPanis <nicolaspanis12@gmail.com>
2023-03-30 09:18:39 +0200

    state V1

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,29 @@ import PokemonCard from './components/PokemonCard.jsx'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 PokemonCard.PropTypes = {
     pokemonList: PropTypes.shape ({
